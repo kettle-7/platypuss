6363b3bd04e2ba0c2bb4b3d5b58dff6af17c545e
upside-down guy <64388266+kettle-7@users.noreply.github.com>
2022-12-14 10:08:09 +1300

    initial commit

---
+++ centralserver/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+
+const hostname = '0.0.0.0';
+const port = 3000;    // Change this variable to change the port for the server
+
