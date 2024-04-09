765e8655a2a85234841de34cb2aec1cacafea2ba
Kettle :~) <64388266+kettle-7@users.noreply.github.com>
2024-04-10 09:44:34 +1200

    change is ? test

--- server/handles/50_message.js
+++ server/handles/50_message.js
@@ -47,18 +47,14 @@ all the information specified in the Platypuss API."
 		}
 		//if (!(/[\!@#$%\^&\*()_+\-=\[\]{};':"\\|,.<>\/?A-Za-z0-9]/.test(packet.message.content)) && !packet) {
 		if (packet.message.content.replace(/[ \t\r\n]/g, "").length < 1) {
-			let skill = true;
-			console.log(packet.message.uploads);
-			if (packet.message.uploads.length) {
-				skill = false;
-			}
-			if (skill)
+			if (packet.message.uploads.length < 1) {
 				packet.ws.send(JSON.stringify({
 					"eventType": "error",
 					"code": "invisibleMsg",
 					"explanation": "An attempt to stop invisible messages.<br><br><pre><code>"+JSON.stringify(packet.message)+"</code></pre>"
 				}));
-			return;
+				return;
+			}
 		}
 		if (packet.ws.lastMessage == packet.message.content && !allowDuplicates) {
 			packet.ws.send(JSON.stringify({
