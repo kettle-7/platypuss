2ad7d35562a39bb6930cf107ead6d81dc6d28f25
kettle-7 <kettle-7@users.noreply.github.com>
2024-04-29 18:06:27 +1200

    don't require a camera

--- cordectropuss/www/vc.js
+++ cordectropuss/www/vc.js
@@ -256,7 +256,7 @@ async function joinRoomById(roomId) {
 
 async function openUserMedia(e) {
   const stream = await navigator.mediaDevices.getUserMedia(
-      {video: true, audio: true});
+      {/*video: true,*/ audio: true});
   document.querySelector('#localVideo').srcObject = stream;
   localStream = stream;
   remoteStream = new MediaStream();
