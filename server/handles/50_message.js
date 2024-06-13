e9e883c4b2162077c511616fd118a8e119f71300
kettle-7 <kettle-7@users.noreply.github.com>
2024-06-13 20:59:08 +1200

    crypto

--- server/handles/50_message.js
+++ server/handles/50_message.js
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ************************************************************************/
 
+const { randomInt } = require("crypto");
 const { v4 } = require("uuid");
 const { } = require("./platypussDefaults.js"); // import nothing :o)
 const fs = require("fs");
