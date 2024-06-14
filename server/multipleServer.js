f6d55a22eefd416b6f73c7cdc1881986592c4924
kettle-7 <kettle-7@users.noreply.github.com>
2024-06-14 17:21:41 +1200

    the port is not a server

--- server/multipleServer.js
+++ server/multipleServer.js
@@ -72,8 +72,7 @@ sdata.properties = conf;
 var handlers = {};
 var clientses = {};
 for (let server in conf) {
-    if (conf[server].authAddr == undefined) conf[server].authAddr = "https://playpuss.net";
-    console.log(conf[server], conf[server].authAddr); // undefined
+    if (conf[server].authAddr == undefined) continue;
     if (conf[server].authAddr.indexOf("http") == 0) {
         conf[server].authAddr = `https://${conf[server].authAddr}`;
     }
