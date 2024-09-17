7c6c6431369907cb0e090bd917f3a8ffe8386cee
kettle-7 <kettle-7@users.noreply.github.com>
2024-09-18 11:27:03 +1200

    more testing

--- server/multipleServer.js
+++ server/multipleServer.js
@@ -204,7 +204,7 @@ const httpser = https.createServer({
             });
             return;
         } else if (url.pathname.startsWith("/uploads")) {
-            url.pathname = decodeURIComponent(url.pathname).replace(/\.\./g, "");
+            url.pathname = decodeURI(url.pathname).replace(/\.\./g, "");
             if (!fs.existsSync("./usercontent"+url.pathname)) {
                 res.writeHead(404, "not found", { "Content-Type": "text/html",
                 "Access-Control-Allow-Origin": "*" });
